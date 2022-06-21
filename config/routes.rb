f47d62728245d5aea9cb16395f24fad337ae5850
EC2 Default User <79982093+mamichiki@users.noreply.github.com>
2022-06-21 15:09:18 +0000

    [Add]

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  root to: 'homes#top'
+  get 'home/about', to: 'homes#about', as: 'about'
+  devise_for :users
+
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  
+  resources :users, only: [:index, :show, :edit, :update]
+  
+  
+  
+
+
+end
